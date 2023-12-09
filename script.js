7a39d5223589f13c977ee9843b7e4f210513eaa0
PHURIT FEUNGFU - 6609650038 <145310722+PHURITfuengfu@users.noreply.github.com>
2023-12-09 20:08:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.getElementById("button1").addEventListener("click", function() {
     window.location.href = './HTML5';
 });
-
-document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = './JS';
-});
